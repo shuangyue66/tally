1a02207f83ac1ae667ccc8a4c95807eebc11f818
shuangyue66 <43638595+shuangyue66@users.noreply.github.com>
2019-11-20 23:24:08 +0800

    记账

---
+++ pages/accounts/index.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="content">
+		<uni-calendar 
+            :insert="true"
+            :lunar="true" 
+            :start-date="'2019-1-1'"
+            :end-date="'2019-5-20'"
+            @change="change"
+            @month-switch="monthSwitch"
+        />
+	</view>
+</template>
+
+<script>
+    import {uniCalendar} from "@/components/uni-calendar/uni-calendar"
+	export default {
+        components: {
+            uniCalendar
+        },
+		data() {
+			return {
+				nowtime: ''
+			}
+		},
+		onLoad() {
+            let date = new Date
+            let Y = date.getFullYear() + '-'
+            let M = date.getMonth() + 1 + '-'
+            let D = date.getDate();
+            let nowtime = Y + M + D
+            this.nowtime = String(nowtime)
+            console.log(String(nowtime), 'nowtime')
+		},
+		methods: {
+			change(e) {
+                console.log(e);
+            },
+            monthSwitch(e) {
+                console.log(e)
+            }
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,90 @@
+<template>
+	<view class="content">
+		<view class="swiper-box">
+			<swiper class="swiper"
+					:indicator-dots="true"
+					:autoplay="true"
+					:interval="2000"
+					:duration="500">
+				<swiper-item>
+					<image class="swiper-item" src="../../static/img1.png" />
+				</swiper-item>
+				<swiper-item>
+					<image class="swiper-item" src="../../static/img2.png" />
+				</swiper-item>
+				<swiper-item>
+					<image class="swiper-item" src="../../static/img3.png" />
+				</swiper-item>
+			</swiper>
+		</view>
+		<view class="text-area">
+			<view @click="onlucky">
+				<icon type="success" size="40"/>
+				<view>点饭</view>
+			</view>
+			<view @click="onAccounts">
+				<icon type="info" size="40"/>
+				<view>记账</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			onlucky(e) {
+				console.log(2)
+			},
+			onAccounts(e) {
+				uni.navigateTo({
+					url: '/pages/accounts/index'
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.swiper-box {
+		width: 600rpx;
+		margin: 60rpx auto 0;
+	}
+
+	.swiper {
+		height: 300rpx;
+	}
+
+	.swiper-item{
+		width: 600rpx;
+		height: 300rpx;
+	}
+
+	.text-area {
+		width: 300rpx;
+		margin: 30rpx 0;
+		display: flex;
+		text-align: center;
+		justify-content: space-between;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
